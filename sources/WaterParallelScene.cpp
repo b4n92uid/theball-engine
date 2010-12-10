3b7bbd2c0b3502da9c8729f659b30778c903fd91
b4n92uid <b4n92uid@localhost>
2010-12-10 12:59:07 +0100

    Imp: Architecture de gestion des noeuds (water)

--- sources/WaterParallelScene.cpp
+++ sources/WaterParallelScene.cpp
@@ -20,6 +20,17 @@ WaterParallelScene::WaterParallelScene()
 
 WaterParallelScene::~WaterParallelScene()
 {
+    Clear();
+}
+
+void WaterParallelScene::Clear()
+{
+    for(unsigned i = 0; i < m_nodes.size(); i++)
+        if(!m_nodes[i]->HasParent())
+            if(!m_nodes[i]->IsLockPtr())
+                delete m_nodes[i], m_nodes[i] = NULL;
+
+    m_nodes.clear();
 }
 
 void WaterParallelScene::PreRender()
@@ -28,7 +39,7 @@ void WaterParallelScene::PreRender()
 
     for(unsigned i = 0; i < m_nodes.size(); i++)
     {
-        Water* water = dynamic_cast<Water*>(m_nodes[i]);
+        Water* water = dynamic_cast<Water*> (m_nodes[i]);
 
         // Render Reflection
         water->BeginReflection();
@@ -60,4 +71,32 @@ void WaterParallelScene::Render()
         m_nodes[i]->Render();
 
     glPopAttrib();
+}
+
+void WaterParallelScene::RegisterWater(Water* water)
+{
+    if(std::find(m_nodes.begin(), m_nodes.end(), water) != m_nodes.end())
+        throw Exception("WaterParallelScene::RegisterWater; child already exist");
+
+    water->SetParallelScene(this);
+
+    m_nodes.push_back(water);
+}
+
+void WaterParallelScene::UnRegisterWater(Water* water, bool deleteptr)
+{
+    Water::Array::iterator it = std::find(m_nodes.begin(), m_nodes.end(), water);
+
+    if(it == m_nodes.end())
+        throw Exception("WaterParallelScene::UnRegisterWater; cannot found child");
+
+    if(deleteptr)
+        delete (*it);
+
+    m_nodes.erase(it);
+}
+
+Iterator<Water*> WaterParallelScene::GetIterator()
+{
+    return Iterator<Water*>(m_nodes);
 }
