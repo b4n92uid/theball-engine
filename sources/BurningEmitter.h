56cc4df8b78bc5b25fe78c042dbfb3dacb003534
Beldjouhri Abdelghani <b4n92uid@gmail.com>
2011-03-29 23:11:35 +0100

    Imp: backward compatibility for particules; split emiter on BurningEmitter subclass

---
+++ sources/BurningEmitter.h
@@ -0,0 +1,84 @@
+/* 
+ * File:   BurningEmitter.h
+ * Author: b4n92uid
+ *
+ * Created on 28 mars 2011, 18:51
+ */
+
+#ifndef BURNINGEMITTER_H
+#define	BURNINGEMITTER_H
+
+
+#include <vector>
+
+#include "Mathematics.h"
+#include "Texture.h"
+
+#include "Node.h"
+#include "Material.h"
+#include "ParticlesParallelScene.h"
+#include "Particles.h"
+
+namespace tbe
+{
+namespace scene
+{
+
+class ParticlesParallelScene;
+
+class BurningEmitter : public ParticlesEmiter
+{
+public:
+    BurningEmitter(ParticlesParallelScene* scene);
+    BurningEmitter(const BurningEmitter& copy);
+    virtual ~BurningEmitter();
+
+    BurningEmitter & operator=(const BurningEmitter& copy);
+
+    Node* Clone();
+    
+    void Process();
+
+    void SetFreeMove(float freeMove);
+    float GetFreeMove() const;
+
+    void SetAutoRebuild(bool autoRebuild);
+    bool IsAutoRebuild() const;
+
+    void SetContinousMode(bool continousMode);
+    bool IsContinousMode() const;
+
+    void SetLifeDown(float lifeDown);
+    float GetLifeDown() const;
+
+    void SetLifeInit(float lifeInit);
+    float GetLifeInit() const;
+
+    void SetGravity(Vector3f gravity);
+    Vector3f GetGravity() const;
+
+    void SetEndPos(Vector3f endPos);
+    Vector3f GetEndPos() const;
+
+    CtorMap ConstructionMap(std::string root);
+
+protected:
+    void SetupBullet(Particle& p);
+
+private:
+    float m_lifeInit;
+    float m_lifeDown;
+    float m_freeMove;
+
+    bool m_continousMode;
+    bool m_autoRebuild;
+
+    Vector3f m_gravity;
+    Vector3f m_endPos;
+};
+
+}
+}
+
+#endif	/* BURNINGEMITTER_H */
+
